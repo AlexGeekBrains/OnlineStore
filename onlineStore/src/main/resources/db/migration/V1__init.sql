1f4205c90390b0cf93c7506eadbf41afe229b35d
Alexander <89163278541@mail.ru>
2022-12-21 20:38:34 +0300

    store part 7

---
+++ onlineStore/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,4 @@
+CREATE TABLE products (id bigserial PRIMARY KEY, title VARCHAR(255), cost INT);
+INSERT INTO products (title, cost)
+VALUES ('Milk', 150), ('Potatoes', 80),('Eggs', 90),('Pancake', 75),('Paper', 150);
+
