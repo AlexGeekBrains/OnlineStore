26616aff1ceab558199b50958f740942b33248fa
Alexander <89163278541@mail.ru>
2022-12-27 22:41:28 +0300

    store part 9

---
+++ onlineStore/src/main/java/ru/alex/repository/specifications/ProductSpecification.java
@@ -0,0 +1,20 @@
+package ru.alex.repository.specifications;
+
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Repository;
+import ru.alex.data.Product;
+
+@Repository
+public class ProductSpecification {
+    public Specification<Product> costGreaterOrEqualsThan(Integer minPrice) {
+        return (Specification<Product>) (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.greaterThanOrEqualTo(root.get("cost"), minPrice);
+    }
+
+    public Specification<Product> costLessOrEqualsThan(Integer maxPrice) {
+        return (Specification<Product>) (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.lessThanOrEqualTo(root.get("cost"), maxPrice);
+    }
+
+    public Specification<Product> titleLike(String titlePart) {
+        return (Specification<Product>) (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.like(root.get("title"), String.format("%%%S%%", titlePart));
+    }
+}
---
+++ onlineStore/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,5 @@
+CREATE TABLE products (id bigserial PRIMARY KEY, title VARCHAR(255), cost INT);
+INSERT INTO products (title, cost)
+VALUES ('Milk', 150), ('Potatoes', 80),('Eggs', 90),('Pancake', 75),('Paper', 150),
+       ('Oil', 50), ('Salt', 10),('Pineapple', 200),('Watermelon', 175),('Candy', 87);
+
